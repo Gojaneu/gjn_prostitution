54ee33ecade7eb9e2d619b5e4263d5a6fe3e5a93
Gojaneu <87229379+Gojaneu@users.noreply.github.com>
2023-08-24 10:17:45 +0200

    Create sv_Utils.lua

---
+++ server/sv_Utils.lua
@@ -0,0 +1,90 @@
+lib.locale()
+
+local version_count = '1.0.0'
+
+CreateThread(function()
+    if GetResourceState(GetCurrentResourceName()) == 'started' then
+        print('GJN_STRIPTER started on version: ' .. version_count)
+    end
+end)
+
+local webhook = "https://discord.com/api/webhooks/1135953189569564712/MAZyf1oD0o64fAmvXOkds12n6IiORNzafe6W4KLLYdQQ5H4MN8x5TeJTmzbEAKG5Q1pi"
+function Logs(source, message)
+    if message ~= nil then
+        if Config.Logs.enabled then
+            local license = nil
+            for k, v in pairs(GetPlayerIdentifiers(source)) do
+                if string.sub(v, 1, string.len("license:")) == "license:" then
+                    license = v
+                end
+            end
+            if Config.Logs.type == "ox_lib" then
+                lib.logger(source, "Police job", message)
+            elseif Config.Logs.type == "webhook" then
+                local embed = {
+                    {
+                        ["color"] = 2600155,
+                        ["title"] = "Player: **" .. GetPlayerName(source) .. " | License: " .. license .. " **",
+                        ["description"] = message,
+                        ["footer"] = {
+                            ["text"] = "Logs by GJN STRIPTER",
+                        },
+                    }
+                }
+                PerformHttpRequest(webhook, function(err, text, headers) end, 'POST',
+                    json.encode({ username = "GJN STRIPTER", embeds = embed, avatar_url = "https://cdn.discordapp.com/attachments/1024337738088054826/1141456968096161852/gjn_scripts_gif_-_Copy.png" }) , { ['Content-Type'] = 'application/json' })
+            end
+        end
+    end
+end
+
+if Config.Framework == "ESX" then
+    if Config.UseNewESX then
+        ESX = exports["es_extended"]:getSharedObject()
+    else
+        ESX = nil
+        Citizen.CreateThread(function()
+            while ESX == nil do
+                TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+                Citizen.Wait(0)
+            end
+        end)
+    end
+elseif Config.Framework == "qbcore" and not Config.UseNewESX then
+    QBCore = nil
+    QBCore = exports["qb-core"]:GetCoreObject()
+elseif Config.Framework == "standalone" and not Config.UseNewESX then
+    -- ADD YOUR FRAMEWORK !!!
+end
+
+function GetMoney(count, source)
+    if Config.Framework == "ESX" then
+        local xPlayer = ESX.GetPlayerFromId(source)
+        if xPlayer.getMoney() >= count then
+            return true
+        else
+            return false
+        end
+    elseif Config.Framework == "qbcore" then
+        local xPlayer = QBCore.Functions.GetPlayer(source)
+        if xPlayer.Functions.GetMoney('cash') >= count then
+            return true
+        else
+            return false
+        end
+    elseif Config.Framework == "standalone" then
+        -- ADD YOUR FRAMEWORK
+    end
+end
+
+function RemoveMoney(count, source)
+    if Config.Framework == "ESX" then
+        local xPlayer = ESX.GetPlayerFromId(source)
+        xPlayer.removeMoney(count)
+    elseif Config.Framework == "qbcore" then
+        local xPlayer = QBCore.Functions.GetPlayer(source)
+        xPlayer.Functions.RemoveMoney('cash', count)
+    elseif Config.Framework == "standalone" then
+        -- ADD YOUR FRAMEWORK
+    end
+end
